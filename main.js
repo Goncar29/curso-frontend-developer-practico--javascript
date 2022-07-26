15882a89f6bdec7a9a580053189049f692014bfe
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-07-26 16:52:32 -0300

    Sea crean archivos css html y js y se enlaza el menu de escritorio

---
+++ main.js
@@ -0,0 +1,8 @@
+const navEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+
+menuEmail = addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive')
+}
