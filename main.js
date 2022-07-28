1f053bcb436ff505f5971dd16718ad4259ad8e57
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-07-28 13:02:47 -0300

    Se agrega el menu de la version mobile

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const navEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail = addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
+
+// https://developer.mozilla.org/es/docs/Web/API/Element/classList
+
+/*  
+    otra forma con arrow function
+const menuDesktop = document.querySelector('.inactive')
+const navEmail = document.querySelector('.navbar-email');
+
+navEmail.addEventListener('click', () => {
+menuDesktop.classList.toggle('inactive');
+}); 
+*/
